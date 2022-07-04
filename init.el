0ce4af7cf90d7e01c6bc6de481ca8c064367e513
lening <zhanglening2001@gmail.com>
2022-07-04 23:02:43 +0900

    test ssh commit in new wsl

--- init.el
+++ init.el
@@ -1,8 +1,8 @@
 ;; ;; Set up package.el to work with MELPA
-;; (require 'package)
+(require 'package)
 ;; (add-to-list 'package-archives
 ;;              '("melpa" . "https://melpa.org/packages/"))
-;; (package-initialize)
+(package-initialize)
 ;; (package-refresh-contents)
 
 ;; ;; Download Evil
